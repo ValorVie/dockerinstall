0127f0d1325390513cea654ab93136ca40290c8c
ValorVie <ValorVie@users.noreply.github.com>
2024-01-13 10:43:08 +0800

    Update fullinstall.sh
    # Dispaly 404: Not Found

--- fullinstall.sh
+++ fullinstall.sh
@@ -75,7 +75,8 @@ for option in "${selected_options[@]}"; do
             COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
             sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
-            sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+            # Dispaly 404: Not Found
+            # curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
             printf '\nDocker Compose installed successfully\n\n'
             log "Docker Compose installed.";;
         "portainer" )
